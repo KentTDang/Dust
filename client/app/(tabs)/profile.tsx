ada4cdcb88b29e9dc843b8dd8f0dd3903d648fe3
KentTDang <kent_2015@outlook.com>
2024-09-15 04:00:25 -0400

    good

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -1,60 +1,87 @@
-import { View, Text, StyleSheet, Button, Image } from 'react-native'
-import React from 'react'
-import { WalletCardIcon } from '@/constants/icons'
-import Colors from '@/constants/Colors'
-import { Stack } from 'expo-router'
-import { auth } from '@/firebaseConfig';
-import { signOut } from 'firebase/auth';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TouchableOpacity } from 'react-native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import React from "react";
+import { WalletCardIcon } from "@/constants/icons";
+import Colors from "@/constants/Colors";
+import { Stack } from "expo-router";
+import { auth } from "@/firebaseConfig";
+import { signOut } from "firebase/auth";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const Page = () => {
-
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-      console.log('User signed out successfully!');
+      console.log("User signed out successfully!");
     } catch (error) {
-      console.error('Error signing out:', error);
+      console.error("Error signing out:", error);
     }
   };
-  
+
   return (
     <>
-    <Stack.Screen options={{headerShown: false}}/>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.wrapper}>
-        <View style={styles.userInfoWrapper}>
-          <Image
-            source={{
-              uri: "https://avatars.githubusercontent.com/u/10240250?v=4",
-            }}
-            style={styles.userImage}
-          />
-          <View style={styles.userTextWrapper}>
-            <Text style={[ styles.userText, {fontSize: 16} ]}>{auth.currentUser?.email}</Text>
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.wrapper}>
+          <View style={styles.userInfoWrapper}>
+            <Image
+              source={{
+                uri:
+                  auth.currentUser?.photoURL ||
+                  "https://avatars.githubusercontent.com/u/10240250?v=4",
+              }}
+              style={styles.userImage}
+            />
+            <View style={styles.userTextWrapper}>
+              <Text style={[styles.userText, { fontSize: 16 }]}>
+                {auth.currentUser?.email}
+              </Text>
+            </View>
           </View>
+
+          <TouchableOpacity onPress={handleSignOut} style={styles.btnWrapper}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <MaterialIcons name="logout" size={16} color={Colors.white} />
+              <Text style={styles.btnText}>Sign Out</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            gap: 10,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          
+          <Text style={{ color: Colors.white, fontSize: 36, fontWeight: 700 }}>
+            $1475.<Text style={{ fontSize: 22, fontWeight: 400 }}>00</Text>
+          </Text>
+          <Text style={{ color: Colors.white, fontSize: 16 }}>
+            Total in Donations
+          </Text>
+        </View>
+        <View>
+
         </View>
 
-        <TouchableOpacity
-          onPress={handleSignOut}
-          style={styles.btnWrapper}
-        >
-          <View style={{flexDirection: "row", alignItems: "center"}}>
-          <MaterialIcons name="logout" size={16} color={Colors.white} />
-          <Text style={styles.btnText}>Sign Out</Text>
+        <View style={{ gap: 20, paddingHorizontal: 20, paddingVertical: 100 }}>
+          <Text style={{ color: Colors.white, fontSize: 20, fontWeight: 700 }}>What's to come?</Text>
+          <View style={{ gap: 20 }}>
+            <Text style={{ color: Colors.white, fontSize: 16, fontWeight: 400 }}>Deductible Tax Summary</Text>
+            <Text style={{ color: Colors.white, fontSize: 16, fontWeight: 400 }}>News Update on Your charities</Text>
+
           </View>
-          
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+        </View>
+      </SafeAreaView>
     </>
-  )
-}
+  );
+};
 
-export default Page
-
+export default Page;
 
 const styles = StyleSheet.create({
   container: {
