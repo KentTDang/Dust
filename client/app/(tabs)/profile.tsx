2e15aabaedc3a591d7fd015f31c22261bef8326d
KentTDang <kent_2015@outlook.com>
2024-09-15 03:09:53 -0400

    header section of profile

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -1,12 +1,13 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, Image } from 'react-native'
 import React from 'react'
 import { WalletCardIcon } from '@/constants/icons'
 import Colors from '@/constants/Colors'
 import { Stack } from 'expo-router'
 import { auth } from '@/firebaseConfig';
 import { signOut } from 'firebase/auth';
-
-
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const Page = () => {
 
@@ -22,29 +23,61 @@ const Page = () => {
   return (
     <>
     <Stack.Screen options={{headerShown: false}}/>
-    <View style={styles.container}>
-      <Text style={styles.text}>profile</Text>
-      <WalletCardIcon width={24} height={24}/>
-      <Button 
-        title="Logout" 
-        onPress={handleSignOut} 
-        color="#e74c3c" 
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.userInfoWrapper}>
+          <Image
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/10240250?v=4",
+            }}
+            style={styles.userImage}
+          />
+          <View style={styles.userTextWrapper}>
+            <Text style={[ styles.userText, {fontSize: 16} ]}>{auth.currentUser?.email}</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleSignOut}
+          style={styles.btnWrapper}
+        >
+          <View style={{flexDirection: "row", alignItems: "center"}}>
+          <MaterialIcons name="logout" size={16} color={Colors.white} />
+          <Text style={styles.btnText}>Sign Out</Text>
+          </View>
+          
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
     </>
   )
 }
 
 export default Page
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: Colors.black,
   },
-  text: {
-    color: Colors.white,
-  }
-})
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 70,
+    paddingHorizontal: 20,
+  },
+  userInfoWrapper: { flexDirection: "row", alignItems: "center" },
+  userImage: { height: 50, width: 50, borderRadius: 30 },
+  btnWrapper: {
+    borderColor: "#666",
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 8,
+  },
+  btnText: { color: Colors.white, marginLeft: 6 },
+  userText: { color: Colors.white },
+  userTextWrapper: { marginLeft: 10 },
+  boldText: { fontWeight: "bold" },
+});
